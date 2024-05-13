4f9a7e27890270437bc1f5b45ac4f5a22ff9b1ba
QianyingLuo <qianyingluodev@gmail.com>
2024-05-13 19:54:43 +0200

    create navbar

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from .controller.user import router as user_router
 from .controller.index import router as index_router
+from .controller.login import router as login_router
 from .repository.mysql_connection import load_database
 
 async def startup_database_loader():
@@ -13,5 +14,6 @@ app.add_event_handler("startup", startup_database_loader)
 
 app.include_router(index_router)
 app.include_router(user_router, prefix="/user")
+app.include_router(login_router, prefix="/login")
 
 
---
+++ src/useful/project.py
@@ -0,0 +1,5 @@
+import os
+
+def get_file_path(file: str) -> str:
+    current_directory = os.path.dirname(__file__)
+    return os.path.join(current_directory, file) 
