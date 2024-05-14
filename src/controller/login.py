1b6d78b0220cb47ad06052c002975669e03c11b7
QianyingLuo <qianyingluodev@gmail.com>
2024-05-14 12:22:51 +0200

    refactor controller login.py

--- src/controller/login.py
+++ src/controller/login.py
@@ -1,24 +1,11 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import FileResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
-
+from fastapi.responses import FileResponse
 from ..useful.project import get_file_path 
 
 router = APIRouter()
-templates = Jinja2Templates(directory="pages")
 
 @router.get("/", response_class=FileResponse)
 async def login_page(request: Request):
     login_path = "../assets/pages/login.html"
     path = get_file_path(login_path)
     return FileResponse(path)
-
-
-
-
-# @router.get("/login")
-# async def login():
-#     current_directory = os.path.dirname(__file__)
-#     login_path = "../assets/pages/login.html"
-#     file_path = os.path.join(current_directory, login_path)
-#     return FileResponse(file_path)
--- src/useful/project.py
+++ src/useful/project.py
@@ -2,4 +2,4 @@ import os
 
 def get_file_path(file: str) -> str:
     current_directory = os.path.dirname(__file__)
-    return os.path.join(current_directory, file) 
+    return os.path.join(current_directory, file)
