e6f79ae3b8a81e10aa6a8725da5c1b84cfa83dd4
QianyingLuo <qianyingluodev@gmail.com>
2024-06-02 19:05:12 +0200

    change category render functions naming and move extendedenvironment class to useful.project

--- src/controller/index.py
+++ src/controller/index.py
@@ -1,29 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import FileSystemLoader
 from ..api import product as product_api
-from ..domain import product as product_domain
+from ..useful.project import ExtendedEnvironment
 
 router = APIRouter()
-
-class ExtendedEnvironment(Environment):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.globals.update({
-            'int': int,
-        })
-    
-    def truncate(self, s, length=120, suffix='...'):
-      if len(s) <= length:
-          return s
-      else:
-          return s[:length].rstrip() + suffix
 
 templates = Jinja2Templates(directory="src/assets/")
 templates.env = ExtendedEnvironment(loader=FileSystemLoader("src/assets/"))
 templates.env.filters['truncate'] = templates.env.truncate
-
 
 @router.get("/", response_class=HTMLResponse)
 def render_index(request: Request): 
